28f9f65629cafdea4711fb9b7a0a63e7038b8341
knor12 <32679571+knor12@users.noreply.github.com>
2023-06-09 16:47:01 +0100

    Add files via upload

---
+++ NKFSMCompiler/NKFSMVersion.py
@@ -0,0 +1,11 @@
+VERSION = "V0.02"
+
+class NKFSMVersion:
+
+    
+
+    def __init__(self):
+        self.version = f"{VERSION}"
+        
+    def getVersion(self):
+        return f'{self.version}'
